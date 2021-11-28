f44352a6b8e2fddf75a373472c9ccd241edb9c1b
Nick Hershberger <hershfamjunk@gmail.com>
2021-11-28 12:52:17 -0500

    day 4 commit

--- day4.py
+++ day4.py
@@ -1,4 +1,22 @@
+from hashlib import md5
+
+
 if __name__ == '__main__':
 
     with open('./inputs/day4.txt') as f:
         hashkey = f.readline()
+
+    f5 = f6 = False
+    for x in range(10000000):
+        value = hashkey + str(x)
+        hash_obj = md5(value.encode())
+        hex = hash_obj.hexdigest()
+        if hex[0:5] == '00000':
+            print(f'{hashkey}, {x}')
+            f5 = True
+        if hex[0:6] == '000000':
+            print(f'{hashkey}, {x}')
+            f6 = True
+
+        if f5 and f6:
+            break
