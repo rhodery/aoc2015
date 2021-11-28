103e14d96830d9138d0c5d197182a71a429f24b6
Nick Hershberger <hershfamjunk@gmail.com>
2021-11-27 23:20:58 -0500

    day 2 commit

--- day2.py
+++ day2.py
@@ -1,8 +1,25 @@
-def calc():
-    return
+def calc(boxes):
+    total = {}
+    paper = 0
+    ribbon = 0
+    for box in boxes:
+        box=list(map(int,box))
+        box.sort()
+        l,w,h = [box[x] for x in (0,1,2)]
+        lw, wh, hl = l*w, w*h, h*l
+        paper += 2*lw + 2*wh + 2*hl + min(lw,wh,hl)
+        ribbon += (l+l+w+w) + (l*w*h)
+    
+    total['paper'] = paper
+    total['ribbon'] = ribbon
+    return total
 
 
 if __name__ == '__main__':
 
     with open('./inputs/day2.txt') as f:
-        boxes = f.readlines()
+        boxes = [line.rstrip('\n').split('x') for line in f.readlines()]
+
+    # part 1 & 2
+    print(calc(boxes)['paper'])
+    print(calc(boxes)['ribbon'])
